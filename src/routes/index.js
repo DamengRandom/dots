4f976dd037b7aa51258e9411bfef846294eb8e13
Damon Wu <damonwu0605@gmail.com>
2019-01-31 23:26:01 +1100

    [DOTS-02][Damon Wu] setup redux and use dmjs react components

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Navbar } from "dmjs-react-components";
 // components
 import { Dashboard } from '../containers/Dashboard';
 import { CreateDot } from '../containers/Dots/CreateDot';
 import { NotFound } from '../containers/NotFound';
 
 const Routes = () => {
+  const title = "Dots ..";
+  const links = [
+    { type: 'route', name: 'Create Dot', url: '/create-dot' },
+    { type: 'icon', target: '_blank', href: 'https://github.com/DamengRandom', icon: 'fab fa-github' },
+  ];
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/" component={Dashboard} exact={true} />
-        <Route path="/create-dot" component={CreateDot} exact={true} />
-        <Route path="*" component={NotFound} />
-      </Switch>
+      <div>
+        <Navbar title={title} links={links} />
+        <Switch>
+          <Route path="/" component={Dashboard} exact={true} />
+          <Route path="/create-dot" component={CreateDot} exact={true} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 }
